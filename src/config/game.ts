2061d64ff9a32be8eb7a9c22008f11d4cea4d684
abbos-21 <abbos.nazariy0621@gmail.com>
2025-11-10 17:41:12 +0500

    commit 75

--- src/config/game.ts
+++ src/config/game.ts
@@ -237,5 +237,5 @@ export const UPGRADABLES_MAX_LEVEL = 13;
 export const COIN_TO_TON_RATE = 135000;
 export const MINIMUM_COIN_WITHDRAWAL = 1000;
 export const MAXIMUM_COIN_WITHDRAWAL = 135000;
-export const CHANNELS = ["@brunoplay_news"];
+export const CHANNELS = ["@brunoplay_news", "@CoinUPuz", "@coinup_projects"];
 export const REWARD_FOR_SUBSCRIPTION = 20;
