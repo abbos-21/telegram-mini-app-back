94595ca5b1358da837576bc8cc4fdb41509f947c
abbos-21 <abbos.nazariy0621@gmail.com>
2025-11-19 09:08:18 +0500

    commit 80

--- src/routes/task.ts
+++ src/routes/task.ts
@@ -96,4 +96,21 @@ router.get("/", async (req: Request, res: Response) => {
   }
 });
 
+router.get("/all", async (req: Request, res: Response) => {
+  try {
+    return res.status(200).json({
+      success: true,
+      data: {
+        tasks: CHANNELS,
+      },
+    });
+  } catch (error) {
+    console.error("Error getting tasks:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+});
+
 export default router;
