4e84f936c38bbf07f06c0615aa9543f05f39e45a
abbos-21 <abbos.nazariy0621@gmail.com>
2025-11-25 20:23:48 +0500

    commit 86

--- src/server.ts
+++ src/server.ts
@@ -14,13 +14,7 @@ export async function startServer() {
   const app = express();
   const server = http.createServer(app);
 
-  app.use(
-    cors({
-      origin: WEB_APP_URL,
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      credentials: true,
-    })
-  );
+  app.use(cors());
 
   app.use(express.json());
 
